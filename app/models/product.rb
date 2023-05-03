345fe98edb7403d4fedd535cfaf352e2c65abffd
bitkoda <jf.liv@pm.me>
2023-05-03 20:17:59 +0100

    add length validation and change default error message for price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,13 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price,
+            numericality: {
+              greater_than_or_equal_to: 0.01,
+              message: 'must be greater than zero and include 2 decimal places'
+            }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true,
                         format: { with: /\.(gif|jpg|png)\z/i,
                                   message: 'must be a URL for GIF, JPG or PNG image.' }
+  validates :title, length: { minimum: 10, maximum: 100 }
 end
