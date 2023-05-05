cb09d72e9f0ba598154c28c254d640d0c54fd60b
bitkoda <jf.liv@pm.me>
2023-05-05 20:40:21 +0100

    add tests for new elements in products index page

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,9 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get products_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select '.price', /\$[,\d]+\.\d\d/
+    assert_select 'nav li .date', /^\d{2}\s\w{3,}\s\d{2}:\d{2}/
   end
 
   test 'should get new' do
