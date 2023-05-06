d43e6b082040f4115bb7059949ad09e5cf364bb0
bitkoda <jf.liv@pm.me>
2023-05-06 15:11:17 +0100

    add cart and line items scaffolds with tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price,
             numericality: {
@@ -10,4 +13,13 @@ class Product < ApplicationRecord
                         format: { with: /\.(gif|jpg|png)\z/i,
                                   message: 'must be a URL for GIF, JPG or PNG image.' }
   validates :title, length: { minimum: 10, maximum: 100 }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    return if line_items.empty?
+
+    errors.add(:base, 'Line items present')
+    throw :abort
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -53,6 +53,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test 'should not be able to delete product in a cart' do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test 'should destroy product' do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
